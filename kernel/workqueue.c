0dcc92b2200d46831b93c6ad25b2c25366723908
sanchitrohilla <sanchitrohilla64@gmail.com>
2018-01-31 04:17:33 +0000

    Revert "fix undeclared func"
    This reverts commit f27c44b4ca32c727bb67d8febf5b675decdf73a1.

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -273,9 +273,6 @@ static cpumask_var_t *wq_numa_possible_cpumask;
 static bool wq_disable_numa;
 module_param_named(disable_numa, wq_disable_numa, bool, 0444);
 
-static bool wq_power_efficient = true;
-module_param_named(power_efficient, wq_power_efficient, bool, 0644);
-
 static bool wq_numa_enabled;		/* unbound NUMA affinity enabled */
 
 /* buf for wq_update_unbound_numa_attrs(), protected by CPU hotplug exclusion */
