1a037059b55fec01a2cb3c54a039115f6d263d96
sanchitrohilla <sanchitrohilla64@gmail.com>
2018-02-04 17:38:52 +0000

    add build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+export CROSS_COMPILE=/home/sanchitrohilla68/aarch64-linux-android-4.9-uber-master/bin/aarch64-linux-android-
+export ARCH=arm64
+export SUBARCH=arm64
+export USE_CCACHE=1
+make lineageos_tomato_defconfig
